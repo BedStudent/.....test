6953dff817fabedd3faac7380e6fd3ba29b4fedd
BedStudent <eitvidasj@gmail.com>
2023-02-17 15:07:55 +0200

    testoju1

--- src/formComponents/login.component.js
+++ src/formComponents/login.component.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export default class Login extends Component {
     render(){
         return (
-            <h1>Login</h1>
+            <h1>Login is here</h1>
         )
     }
 }
